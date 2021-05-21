9a1b1e32572ec76aa63948f4281c9be0454b7b73
Ashish <ashish.ojha@xoriant.com>
2021-05-22 01:04:37 +0530

    Added: src/test.ts

---
+++ src/test.ts
@@ -0,0 +1,38 @@
+// This file is required by karma.conf.js and loads recursively all the .spec and framework files
+
+import 'zone.js/dist/long-stack-trace-zone';
+import 'zone.js/dist/proxy.js';
+import 'zone.js/dist/sync-test';
+import 'zone.js/dist/jasmine-patch';
+import 'zone.js/dist/async-test';
+import 'zone.js/dist/fake-async-test';
+import { getTestBed } from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting
+} from '@angular/platform-browser-dynamic/testing';
+
+// Unfortunately there's no typing for the `__karma__` variable. Just declare it as any.
+declare const __karma__: any;
+declare const require: any;
+
+// Prevent Karma from running prematurely.
+__karma__.loaded = function () {};
+
+// First, initialize the Angular testing environment.
+getTestBed().initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting()
+);
+// Then we find all the tests.
+// Ori
+const context = require.context('./', true, /\.spec\.ts$/);
+// Ori-
+// Add
+// const context = require.context('./src/app/dashboard/', true, /\.spec\.ts$/);
+// Add-
+// And load the modules.
+context.keys().map(context);
+console.log("context: ", context, " context.keys: ", context.keys() );
+// Finally, start Karma to run the tests.
+__karma__.start();
